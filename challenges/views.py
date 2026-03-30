69548c0c1c0be0c798f40b1b24ebe485e89d7aed
ti-vo <teresa.vogl@uni-leipzig.de>
2026-03-30 10:24:22 +0200

    first minimum working example

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+
+challenges_dict = {
+    "january":"",
+    "february": "",
+    "march": ""
+}
 
 # Create your views here.
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
+
+def monthly_challenges(request, month):
+    challenge_text = challenges_dict.get(month)
+    return HttpResponse(challenge_text)
+    #return HttpResponseNotFound("this month is not supported")
